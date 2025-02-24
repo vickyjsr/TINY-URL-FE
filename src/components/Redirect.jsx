cf259280aec281cac58f6390e5dc7152eb93a2b3
vickyjsr <gouravmodi10@gmail.com>
2025-02-24 11:43:32 +0530

    changes

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../services/api';
+import '../styles/Redirect.css';
 
 const Redirect = () => {
     const { code } = useParams();
